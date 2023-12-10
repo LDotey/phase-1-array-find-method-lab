3f18eb5184489e21f5bd137e3a7e1e752a5dfcbb
Leigh Dotey <leighdotey@gmail.com>
2023-12-10 16:30:01 -0500

    find method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+
+
+
+// function superbowlWin(record){
+//     if (record.result === "W"){
+//     return record.year;
+//     }
+// }
+//     const broncosWin = record.find((superbowlWin) => superbowlWin === "W");
+//     console.log (broncosWin);
+    function superbowlWin(record) {
+        const broncosWin = record.find(obj => obj.result === "W");
+        if (broncosWin) {
+          return broncosWin.year;
+        }
+      }
